7e1e52bcf0117422d23747fcab8ae29ab1145f4d
Nkosingiphile <phile105@gmail.com>
2025-08-14 20:00:20 +0200

    Updated the logo and the footer link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.png';
-import './App.css';
-import Dictionary from './Dictionary';
+import books from "./books.jpg";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <img src={books} className="App-books img-fluid" alt="books" />
         </header>
         <main>
           <Dictionary defaultKeyword="flowers" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Nkosingiphile Mbhele</small>
+          <small>
+            This project was coded by Nkosingiphile Mbhele and is{" "}
+            <a href="https://github.com/Phile105/dictionary-project">
+              open sourced in GitHub{" "}
+            </a>{" "}
+            and hosted on{" "}
+            <a href="https://dictionary-word-app.netlify.app/">Netlify</a>{" "}
+          </small>
         </footer>
       </div>
     </div>
   );
 }
-
-
